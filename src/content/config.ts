af3231c01ddfd9c2937b8682fd87545675ed30a3
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-04-17 02:47:00 -0300

    initial config

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { z, defineCollection } from "astro:content"
+
+const recipesCollection = defineCollection({
+  type: "content",
+  schema: {
+    title: z.string(),
+    description: z.string(),
+    preparationTime: z.object({
+      total: z.string(),
+      preparation: z.string(),
+      cooking: z.string(),
+    }),
+    ingredients: z.array(z.string()),
+    instructions: z.array(z.object({ title: z.string(), process: z.string() })),
+    nutrition: z.array(z.object({ key: z.string(), value: z.string() })),
+  },
+})
+
+export const collections = {
+  recipes: recipesCollection,
+}
